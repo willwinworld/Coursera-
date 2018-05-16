97948dc6cbc49e5f0518894e94a5e1381884d14e
willwinworld <zhangzhouit@163.com>
2018-05-16 14:01:11 +0800

    LaplaceUnigramLanguageModel LaplaceBigramLanguageModel

--- Natural-Language-Processing-Stanford-Coursera/Homework2/CMP462 HW02 Data/python/LaplaceBigramLanguageModel.py
+++ Natural-Language-Processing-Stanford-Coursera/Homework2/CMP462 HW02 Data/python/LaplaceBigramLanguageModel.py
@@ -7,8 +7,9 @@ class LaplaceBigramLanguageModel:
   def __init__(self, corpus):
     """Initialize your data structures in the constructor."""
     # TODO your code here
-    self.biGramCounts = collections.defaultdict(lambda: 0)
-    self.uniGramCounts = collections.defaultdict(lambda: 0)
+    self.laplaceBigramCounts = collections.defaultdict(lambda: 0)
+    self.words = collections.defaultdict(lambda: 0)
+    self.total = 0
     self.train(corpus)
 
   def train(self, corpus):
@@ -17,29 +18,39 @@ class LaplaceBigramLanguageModel:
     """  
     # TODO your code here
     for sentence in corpus.corpus:
-        datums = sentence.data
-        for index in range(len(datums)):
-            token_i = datums[index].word
-            # count C(W_i_1)
-            self.uniGramCounts[token_i] += 1
-            if index > 0:
-                # count C(W_i_1, W_i)
-                token_i_1 = datums[index - 1].word
-                bi_key = token_i_1 + "," + token_i
-                self.biGramCounts[bi_key] += 1
+        token1 = '<s>'
+        self.words[token1] = self.words[token1] + 1
+        token2 = ''
+        for datum in sentence.data:
+            token2 = datum.word
+            self.total += 1
+            self.laplaceBigramCounts[(token1, token2)] = self.laplaceBigramCounts[(token1, token2)] + 1
+            self.words[token2] = self.words[token2] + 1
+            token1 = datum.word
+        token2 = '</s>'
+        self.laplaceBigramCounts[(token1, token2)] = self.laplaceBigramCounts[(token1, token2)] + 1
+        self.words[token2] = self.words[token2] + 1
 
   def score(self, sentence):
     """ Takes a list of strings as argument and returns the log-probability of the 
         sentence using your language model. Use whatever data you computed in train() here.
     """
     # TODO your code here
-    score = 0
-    v = 0
-    for i in range(len(sentence)):
-        if i > 0:
-            bi_key = sentence[i - 1] + "," + sentence[i]
-            count_bigram = self.biGramCounts[bi_key]
-            count_unigram = self.uniGramCounts[sentence[i - 1]]
-            score += math.log(count_bigram + 1)
-            score -= math.log(count_unigram + len(self.biGramCounts))
+    score = 0.0
+    token1 = '<s>'
+    token2 = ''
+    for word in sentence:
+        token2 = word
+        count = self.laplaceBigramCounts[(token1, token2)]
+        if count > 0:
+            score += math.log(count + 1)
+        score -= math.log(self.words[token1] + len(self.words))
+        token1 = word
+
+    token2 = '</s>'
+    count = self.laplaceBigramCounts[(token1, token2)]
+    if count > 0:
+        score += math.log(count + 1)
+    score -= math.log(self.words[token1] + len(self.words))
+
     return score
